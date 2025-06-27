1b2c058e2e8dec74fd1fff5f3c82054be5b338c4
Francis Addo <francis.t.addo@gmail.com>
2025-06-27 18:48:17 +0100

    #1) Add Reverse proxy middle ware

--- YARP.Demos/YARP.As.Migration/Program.cs
+++ YARP.Demos/YARP.As.Migration/Program.cs
@@ -5,10 +5,13 @@ namespace YARP.As.Migration
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            // Add services to the container.
+            builder.Services.AddReverseProxy()
+            .LoadFromConfig(builder.Configuration.GetSection("YARP"));
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
-
+            //app.MapGet("/", () => "Hello World!");
+             app.MapReverseProxy();
             app.Run();
         }
     }
